4c191f702283c3caeb94d8d1a0592304a3046740
Ciara Anderson <thelaylinguist@gmail.com>
2020-06-25 17:06:59 -0600

    Add Patch routes

--- src/index.js
+++ src/index.js
@@ -46,6 +46,28 @@ app.get('/users/:id', async (req, res) => {
     }
 })
 
+app.patch('/users/:id', async (req,res) => {
+    const updates = Object.keys(req.body)
+    const allowedUpdates = ['name', 'email', 'password', 'age']
+    const isValidOperation = updates.every((update) => allowedUpdates.includes(update))
+    
+    if (!isValidOperation) {
+        return res.status(400).send({ error: 'Invalid updates!' })
+        }
+    
+    try {
+        const user = await User.findByIdAndUpdate(req.params.id, req.body, { new: true, runValidators: true })
+
+        if (!user) {
+            return res.status(404).res.send()
+        }
+
+        res.send(user)
+    } catch (e) {
+        res.status(400).send(e)
+    }
+})
+
 app.post('/tasks', async (req, res) => {
     const task = new Task(req.body)
 
@@ -82,6 +104,30 @@ app.get('/tasks/:id', async (req, res) => {
     }
     
 })
+
+
+app.patch('/tasks/:id', async (req,res) => {
+    const updates = Object.keys(req.body)
+    const allowedUpdates = ['description', 'completed']
+    const isValidOperation = updates.every((update) => allowedUpdates.includes(update))
+    
+    if (!isValidOperation) {
+        return res.status(400).send({ error: 'Invalid updates!' })
+        }
+    
+    try {
+        const task = await Task.findByIdAndUpdate(req.params.id, req.body, { new: true, runValidators: true })
+
+        if (!task) {
+            return res.status(404).res.send()
+        }
+
+        res.send(task)
+    } catch (e) {
+        res.status(400).send(e)
+    }
+})
+
 app.listen(port, () => {
     console.log('Server is up on port ' + port)
 })
