5b9876e9b395fb4f90ef94924405618a67ee319b
Ciara Anderson <thelaylinguist@gmail.com>
2020-06-29 11:16:30 -0600

    Generating Authentication Tokens

--- src/routers/task.js
+++ src/routers/task.js
@@ -52,7 +52,10 @@ router.patch('/tasks/:id', async (req,res) => {
         }
     
     try {
-        const task = await Task.findByIdAndUpdate(req.params.id, req.body, { new: true, runValidators: true })
+        const task = await Task.findByIdAndUpdate(req.params.id)
+
+        updates.forEach((update) => task[update] = req.body[update])
+        await task.save()
 
         if (!task) {
             return res.status(404).res.send()
@@ -62,6 +65,8 @@ router.patch('/tasks/:id', async (req,res) => {
     } catch (e) {
         res.status(400).send(e)
     }
+
+
 })
 
 router.delete('/tasks/:id', async (req, res) => {
